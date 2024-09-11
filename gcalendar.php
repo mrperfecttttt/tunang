3e805ebdcbbe1ab9dfb3cc8a9c614b9bad16852e
mrperfecttttt <wanmuhammadfikri@graduate.utm.my>
2024-09-11 08:13:51 +0800

    test calendar

---
+++ gcalendar.php
@@ -0,0 +1,19 @@
+<?php
+// Function to generate Google Calendar URL
+function generateGoogleCalendarUrl() {
+    $eventStart = '20240501T100000Z';  // Replace with your event start time in UTC (Ymd\THis\Z)
+    $eventEnd = '20240501T140000Z';    // Replace with your event end time in UTC (Ymd\THis\Z)
+    $eventName = urlencode("Majlis Perkahwinan Fikri & Syahirah");
+    $eventDescription = urlencode("You're invited to the wedding event of Fikri & Syahirah!");
+    $eventLocation = urlencode("Wedding Venue, Kuala Lumpur, Malaysia");
+
+    $googleCalendarUrl = "https://www.google.com/calendar/render?action=TEMPLATE";
+    $googleCalendarUrl .= "&text=$eventName";
+    $googleCalendarUrl .= "&dates=$eventStart/$eventEnd";
+    $googleCalendarUrl .= "&details=$eventDescription";
+    $googleCalendarUrl .= "&location=$eventLocation";
+    $googleCalendarUrl .= "&sf=true&output=xml";
+
+    return $googleCalendarUrl;
+}
+?>
